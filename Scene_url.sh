c41ded33f7f0dec66cd1f16b8214b78fd92bf9c1
Bugme7-Coolapk <60341871+Bugme7-Coolapk@users.noreply.github.com>
2020-02-12 08:42:03 +0800

    Update Scene_url.sh

--- Scene_url.sh
+++ Scene_url.sh
@@ -1,1 +1,11 @@
-wget -P $MOD_FILES_DIR/Camera/Scene/ http://viptoyou.cn/b38/MiuiCamera.apk
+#
+# 模块(小米6 补全计划)功能下载配置文件
+# 作者&编写:酷安@Bugme7
+# 未经允许，严禁二改/二传
+#
+# 文件大小
+download_file_size_4_5="9.47MB"
+# 创建下载脚本
+touch $MOD_FILES_DIR/Camera/Scene/Download/Scene_url.sh
+# 向下载脚本写入下载网址
+echo "wget -P $MOD_FILES_DIR/Camera/Scene/ http://viptoyou.cn/b38/MiuiCamera.apk" >> $MOD_FILES_DIR/Camera/Scene/Download/Scene_url.sh
