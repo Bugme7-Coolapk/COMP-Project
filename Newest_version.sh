796226d255263eb4d5a851555905575ba457ed9b
Bugme7-Coolapk <60341871+Bugme7-Coolapk@users.noreply.github.com>
2020-02-12 09:00:07 +0800

    Create Newest_version.sh

---
+++ Newest_version.sh
@@ -0,0 +1,19 @@
+#
+# 模块(小米6 补全计划)更新配置文件
+# 作者&编写:酷安@Bugme7
+# 未经允许，严禁二改/二传
+#
+# 更新模块版本
+newest_module_version="2.8"
+# 更新模块大小
+download_size_newest_version="98.26MB"
+# 更新模块MD5值
+module_update_md5="d82a73f5f863b7ca53b9dac1d44f38d7"
+# 更新模块名称
+update_module_name="[Magisk]$newest_module_version"
+# 更新强制
+newest_module="非强制更新"
+# 创建更新网址脚本
+touch $TMPDIR/Newest_version_url.sh
+# 往更新网址脚本写入更新网址
+echo "wget -P $download_update_path https://viptoyou.cn/b38/[Magisk]2.8.zip" >> $TMPDIR/Newest_version_url.sh
