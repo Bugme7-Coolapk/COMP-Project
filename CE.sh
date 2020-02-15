840f30e7be62ab33c863dd4e157164f2f70ddb8a
Bugme7-Coolapk <60341871+Bugme7-Coolapk@users.noreply.github.com>
2020-02-15 14:20:50 +0800

    Add files via upload

--- CE.sh
+++ CE.sh
@@ -3,9 +3,12 @@
 # 作者&编写:酷安@Bugme7
 # 未经允许，严禁二改/二传
 #
-# 文件大小
+# 文件信息
+## 文件大小
 download_file_size_CE="8.40MB"
+## 文件MD5值
+Scene_md5="2ef7a15f3e7f878cd502b176c707dc76"
 # 创建下载脚本
 touch $MOD_FILES_DIR/Download/CE_url.sh
 # 向下载脚本写入下载网址
-echo "wget -P $MOD_FILES_DIR/Camera/Scene/ http://viptoyou.cn/b38/apk/ContentExtension.apk" >> $MOD_FILES_DIR/Download/CE_url.sh
+echo "wget -P $MOD_FILES_DIR/Download/ http://viptoyou.cn/b38/apk/ContentExtension.apk" >> $MOD_FILES_DIR/Download/CE_url.sh
