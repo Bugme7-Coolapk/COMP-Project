90f242e3f267745c2f8126f26335357e84b18dda
Bugme7-Coolapk <60341871+Bugme7-Coolapk@users.noreply.github.com>
2020-02-15 14:18:47 +0800

    Update and rename Scene_url.sh to Scene.sh

--- Scene.sh
+++ Scene.sh
@@ -3,8 +3,11 @@
 # 作者&编写:酷安@Bugme7
 # 未经允许，严禁二改/二传
 #
-# 文件大小
+# 文件信息
+## 文件大小
 download_file_size_4_5="9.47MB"
+## 文件MD5值
+Scene_md5="a4449fdb91376ff3ef6d29d7f20161b5"
 # 创建下载脚本
 touch $MOD_FILES_DIR/Camera/Scene/Download/Scene_url.sh
 # 向下载脚本写入下载网址
